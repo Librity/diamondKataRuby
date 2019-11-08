e00b1525e11a6aadc97e986437e6a3b3a0c7ec4a
Luis Geniole <luis.geniole@gmail.com>
2019-11-07 21:23:48 -0300

    done

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
-source "https://rubygems.org"
+
+source 'https://rubygems.org'
 ruby '>= 2.3.0'
-gem 'rspec' 
+gem 'rspec'
--- lib/diamond.rb
+++ lib/diamond.rb
@@ -1,18 +1,30 @@
+# frozen_string_literal: true
+
+# A class that makes Diamonds & stuff
 class Diamond
+  LETTERS = ('A'..'Z').to_a
 
-  def build input
-    diamond
+  def build(letter)
+    (+letter).upcase!.freeze
+    index = LETTERS.index(letter)
+    'Invalid parameters!' unless index
 
-    diamond_index =(input.upcase.to_s.ord - 65)*2+1
-    if diamond_index == 1
-      puts input
+    aux = (index + 1) * 2
+    total_columns = aux.odd? ? aux : aux - 1
+    diamond = ''
+    row = '_' * total_columns
+
+    (index + 1).times do |row_number|
+      first_letter_position = index - row_number
+      last_letter_position = (total_columns - 1) - (index - row_number)
+
+      row[first_letter_position] = LETTERS[row_number]
+      row[last_letter_position] = LETTERS[row_number] unless row_number.zero?
+
+      diamond += row + "\n"
+      row = '_' * total_columns
     end
 
-    diamond_index.times do |i|
-      
-    end
-
-    puts diamond
+    diamond + diamond.split("\n")[0..-2].reverse.map { |l| l + "\n" }.join
   end
-
 end
